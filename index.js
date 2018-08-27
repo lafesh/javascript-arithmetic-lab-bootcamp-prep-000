1869ad0539e6976c8d9890dda529886ab77f9a34
Gina Fedder <ginafedder@yahoo.com>
2018-08-27 17:07:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var a, b
+
+function add() {
+ add(a, b);
+}
+
+function subtract(a, b) {
+  subtract(a, b);
+}
+
+function multiply() {
+  multiply (a, b);
+}
+
+function divide() {
+  divide (a,b)
+}
